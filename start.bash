dc8ac8434960f23733cca684fd32c96a44da1e27
Andrew Lee <alee4738@gmail.com>
2021-08-10 18:02:58 -0700

    script to start server easily

--- start.bash
+++ start.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-if [ ! -d "wwwroot" ]; then
+project_root_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+if [ ! -d "$project_root_dir/wwwroot" ]; then
   echo "Website does not exist. Building..."
-  pushd frontend > /dev/null
+  pushd "$project_root_dir/frontend" > /dev/null
   ng build
   popd > /dev/null
 fi
 
-node index.js
+node "$project_root_dir/index.js"
